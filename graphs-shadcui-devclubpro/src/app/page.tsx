1dfedf5f018ef03e78bff75ce2e23ef342f15475
Seu Nome <seuemail@exemplo.com>
2024-11-06 18:14:23 -0300

    start setup

---
+++ graphs-shadcui-devclubpro/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <h1>ShadcnUi Charts</h1>
+    </div>
+  );
+}
